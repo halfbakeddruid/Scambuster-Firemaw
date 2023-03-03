fdd6ccbf150b979b4e853f8ee2f4654a858536d9
halfbakeddruid <126875403+halfbakeddruid@users.noreply.github.com>
2023-03-03 17:26:25 +0000

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@
 --=========================================================================================
 local _, t = ...
 t.my_name = "Firemaw Discord Blacklist"
-t.my_provider = "Firemaw EU Duscord"
+t.my_provider = "https://discord.gg/yB9eFn4PDZ"
 t.my_realm = "Firemaw"
 t.my_description = "Realm discord Blacklist for the Firemaw EU server."
 t.my_url = "https://discord.gg/yB9eFn4PDZ"
