9777a3ce7b1b1586afa0f6cb965099a6a6c2c911
halfbakeddruid <alexfromtrafford@gmail.com>
2024-06-21 15:40:20 +0100

    Update core.lua

--- core.lua
+++ core.lua
@@ -4,14 +4,14 @@
 local _, t = ...
 local provider_table = {
     name = t.my_name,
-    provider = t.my_provider,
+    provider = t.my_provider, -- organisation name
     description = t.my_description,
-    url = t.my_url,
+    url = t.my_url, -- organisation url
     realm_data = {
         [t.my_realm] = t.case_table
     }
 }
 
--- Register the list with cutpurse.
+-- Register the list with Scambuster.
 local SB = LibStub("AceAddon-3.0"):GetAddon("Scambuster")
 SB:register_case_data(provider_table)
